cc5239c9c8d2edd7617d3089105e798311ff3192
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-03-22 14:49:33 +0100

    Add field and Tax method.

--- App1/App1/State.cs
+++ App1/App1/State.cs
@@ -8,5 +8,37 @@ namespace App1
 {
     class State
     {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        private double groceries;
+        private double preparedFood;
+        private double prescriptionDrug;
+        private double nonPrescriptedDrug;
+        private double clothing;
+
+        public double Tax(ProductType type)
+        {
+            switch (type)
+            {
+                case ProductType.Clothing:
+                    return clothing;
+                case ProductType.Groceries:
+                    return groceries;
+                case ProductType.NonPrescriptionDrug:
+                    return nonPrescriptedDrug;
+                case ProductType.PreparedFood:
+                    return preparedFood;
+                case ProductType.PrescriptionDrug:
+                    return prescriptionDrug;
+                default:
+                    throw new Exception();
+            }
+        }
     }
 }
