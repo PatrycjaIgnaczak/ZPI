7bd1c70948a5a2afaf38ecc9b9b20eec90afa4fe
Grzegorz Banach <180311@edu.p.lodz.pl>
2017-03-22 15:11:27 +0100

    mock

---
+++ App1/App1/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Product
+    {
+        private const string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        private const ProductType type;
+        private double price;
+
+        public Product(string name, ProductType type, double price)
+        {
+            this.name = name;
+            this.type = type;
+            this.price = price;
+        }
+        public double PriceAfterTax(State state)
+        {
+            double tax = State.Tax(type) / 100;
+            return price * (1 + tax);
+        }
+        public double PriceAfterDiscount()
+        {
+            return price;
+        }
+    }
+}
