902255ff440ae020dbe4a98399d1cb5526fb73cf
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-04-05 14:52:33 +0200

    BUGFIX Change type Value property.

--- App1/App1/ComboboxItem.cs
+++ App1/App1/ComboboxItem.cs
@@ -9,7 +9,7 @@ namespace App1
     class ComboboxItem
     {
         public string Text { get; set; }
-        public string Value { get; set; }
+        public State Value { get; set; }
 
         public override string ToString()
         {
