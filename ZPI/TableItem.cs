8949f2ade29caaadc24d780baec56909af2efda4
Szczepaniak.Wojciech <195067@edu.p.lodz.pl>
2017-05-10 15:22:14 +0200

    Dodanie klasy definiującej rekord w tabeli.

---
+++ ZPI/TableItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPI
+{
+    class TableItem
+    {
+        public string State;
+
+        public double AfterTaxation;
+
+        public double MarkUp;
+    }
+}
